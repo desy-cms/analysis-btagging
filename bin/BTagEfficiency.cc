690f87ef0344c89ee3fa75c7c0aae3f6f83a01ab
Roberval Walsh <roberval.walsh@gmail.com>
2019-08-26 18:21:27 +0200

    more appropriate codes for btagging efficiencies

---
+++ bin/BTagEfficiency.cc
@@ -0,0 +1,52 @@
+#include "Analysis/BTagging/interface/BTaggingAnalyser.h"
+
+// this example has no selection
+// for MC it will apply the generator weights
+
+using namespace std;
+using namespace analysis;
+using namespace analysis::tools;
+using namespace analysis::btagging;
+
+int main(int argc, char ** argv)
+{
+   BTaggingAnalyser analyser(argc,argv);
+   auto config = analyser.config();
+   
+// HISTOGRAMS definitions  
+   // create some predefined jet histograms
+   // if not defined, the number of jets is nJetMin from the configurations
+   analyser.jetHistograms("kinematics");
+   analyser.jetHistograms("jet1btag");
+   analyser.jetHistograms("jet2btag");
+   
+   for ( int i = 0 ; i < analyser.nEvents() ; ++i )
+   {
+      if ( ! analyser.event(i)                  )   continue;
+      
+      analyser.actionApplyPileupWeight();                      // correction : pileup reweight
+   // JETS
+      analyser.actionApplyJER();                               // correction : jet energy resolution smearing
+      analyser.actionApplyBjetRegression();                    // correction : jet energy regression (for b jets)
+      if ( ! analyser.selectionJetId()          )   continue;  // selection  : jet identification 
+      if ( ! analyser.selectionJetPileupId()    )   continue;  // selection  : jet Pileup identification 
+      if ( ! analyser.selectionNJets()          )   continue;  // selection  : number of jets 
+      if ( ! analyser.selectionJet(1)           )   continue;  // selection  : jet1 pt and eta 
+      if ( ! analyser.selectionJet(2)           )   continue;  // selection  : jet2 pt and eta 
+      if ( ! analyser.selectionJetDphi(1,2)     )   continue;  // selection  : delta_phi_jets (1,2) [or  MIN(neg): analyser.selectionJetDphi(1,2,-2.0) / MAX(pos): analyser.selectionJetDphi(1,2,+2.0)]
+      analyser.fillJetHistograms("kinematics");                // histograms : jets fill
+   // BTAG - test of btag only, no selection
+      for ( int ib = 0; ib < config->nBJetsMin(); ++ib )
+      {
+         if (   analyser.selectionBJet(ib)          )
+         {
+            float sf = analyser.getBtagSF(ib);  // only retrieve scale factor to be applied to histograms below
+            analyser.fillJetHistograms(ib,Form("jet%dbtag",ib),sf);                  // histograms : jet 1 fill weighting by the SF
+         }
+      }
+      
+   }  //end event loop
+   
+
+} // end main
+      
