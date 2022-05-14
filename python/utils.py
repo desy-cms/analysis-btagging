8dcbce23449102bce8df74cd6f12df77c8345f31
Roberval Walsh <roberval.walsh@gmail.com>
2022-05-14 08:15:31 +0200

    btag weight analysis

--- python/utils.py
+++ python/utils.py
@@ -1,7 +1,177 @@
 import os
 import glob
-from ROOT import TFile, TH1F, TH2F, TGraphAsymmErrors
+from ROOT import TFile, TH1F, TH2F, TGraphAsymmErrors, TH1
 from Analysis.Tools.utils import Process
+
+TH1.SetDefaultSumw2()
+
+def sample_info(mydir):
+   # sample info from directories with name like btag_weight_deepjet_medium_fh_2018_notrg
+   info = {}
+   info['trg']   = mydir.split('_')[-1]
+   info['year']  = mydir.split('_')[-2]
+   info['mode']  = mydir.split('_')[-3]
+   info['bwp']   = mydir.split('_')[-4]
+   info['balgo'] = mydir.split('_')[-5]
+   return info
+
+
+      # this class has to prepare pt, eta, phi and possibly m12 distributions, flavour dependent?
+class BTagWeight:
+   def __init__(self,process,histo_dir='kinematics',flavours=['b','bb','c','cc','udsg'],variables=['pt','eta','phi']):
+      self.m_proc = process
+      self.m_histo_dir = histo_dir
+      self.m_flvs = flavours
+      self.m_vars = variables
+      self.m_jets = []
+      self.m_dijets = []
+      self.m_histos = {}
+      self.m_histos_dijet = {}
+      self.m_histos_proc = {}
+      self.m_histos_dijet_proc = {}
+      
+   def histogram_directory(self):
+      return self.m_histo_dir
+      
+   def flavours(self):
+      return self.m_flvs
+      
+   def histograms(self,process=False):
+      if process:
+         return self.m_histos_proc
+      return self.m_histos
+      
+   def histograms_dijet(self,process=False):
+      if process:
+         return self.m_histos_dijet_proc
+      return self.m_histos_dijet
+      
+   def jets(self):
+      return self.m_jets
+      
+   def dijets(self):
+      return self.m_dijets
+      
+   # read histograms from the files   
+   def readTH1(self,lumi_scale=False):
+      h1 = {}
+      pbins = []
+      jets = []
+      njets = -1
+      h_types = self.m_vars
+      flv_all = self.flavours()
+      flv_all.append('all')
+      for pbin, pfile in self.m_proc.rootFiles().items():  # need to think better these loops to avoid more loops below
+         ls = self.m_proc.luminosityScale()[pbin]
+         h1[pbin] = {}
+         pbins.append(pbin)
+         tfile = TFile( pfile, 'OLD' )
+         # get number of jets
+         h_dir = tfile.GetDirectory(self.m_histo_dir)
+         h_dir_keys = h_dir.GetListOfKeys()
+         histo_names = {}
+         for typ in h_types:
+            histo_names[typ] = [x.GetName() for x in h_dir_keys if typ+'_jet' in x.GetName() and x.GetName().count('_')==1 and len(x.GetName())==(len(typ)+5)]
+         if not jets:  # number of jets is the same for all process bins
+            njets = len(histo_names['pt'])
+            for j in range(1,njets+1):
+               jet = 'jet{}'.format(j)
+               jets.append(jet)
+         for flv in flv_all:
+            h1[pbin][flv]={}
+            for jet in jets:
+               h1[pbin][flv][jet]={}
+               for typ in h_types:
+                  h_name = '{}_{}_{}'.format(typ,jet,flv)
+                  if flv == 'all':
+                     h_name = '{}_{}'.format(typ,jet)
+                  h1[pbin][flv][jet][typ] = h_dir.Get(h_name)
+                  h1[pbin][flv][jet][typ].SetName('{}_{}'.format(h_name,pbin))
+                  h1[pbin][flv][jet][typ].SetDirectory(0) # Important!
+                  if lumi_scale:
+                     h1[pbin][flv][jet][typ].Scale(ls)
+         tfile.Close()
+         
+      self.m_histos_proc = h1
+      self.m_jets = jets
+         
+      h1_add_proc = {}
+      for f,flv in enumerate(flv_all):
+         h1_add_proc[flv] = {}
+         for jet in jets:
+            h1_add_proc[flv][jet] = {}
+            for typ in h_types:
+               h_name = typ+'_'+jet+'_'+flv
+               for b,pbin in enumerate(pbins):
+                  if b == 0:
+                     h1_add_proc[flv][jet][typ] = h1[pbin][flv][jet][typ].Clone(h_name)
+                  else:
+                     h1_add_proc[flv][jet][typ].Add(h1[pbin][flv][jet][typ])
+                  h1_add_proc[flv][jet][typ].SetDirectory(0) # Important!
+               
+               
+      self.m_histos = h1_add_proc
+      
+   # read histograms from the files   
+   def readTH1Dijet(self,lumi_scale=False):
+      h1 = {}
+      pbins = []
+      jets = []
+      njets = -1
+      h_types = self.m_vars
+      flv_all = self.flavours()
+      flv_all = ['all']
+      for pbin, pfile in self.m_proc.rootFiles().items():  # need to think better these loops to avoid more loops below
+         ls = self.m_proc.luminosityScale()[pbin]
+         h1[pbin] = {}
+         pbins.append(pbin)
+         tfile = TFile( pfile, 'OLD' )
+         # get number of jets
+         h_dir = tfile.GetDirectory(self.m_histo_dir)
+         h_dir_keys = h_dir.GetListOfKeys()
+         histo_names = {}
+         for typ in h_types:
+            histo_names[typ] = [x.GetName() for x in h_dir_keys if typ+'_jet' in x.GetName() and x.GetName().count('_')==1 and len(x.GetName())==(len(typ)+6)]
+         if not jets:  # number of jets is the same for all process bins
+            for j2 in histo_names['m']:
+               jets.append(j2[2:])
+            njets = len(jets)
+         for flv in flv_all:
+            h1[pbin][flv]={}
+            for jet in jets:
+               h1[pbin][flv][jet]={}
+               for typ in h_types:
+                  h_name = '{}_{}_{}'.format(typ,jet,flv)
+                  if flv == 'all':
+                     h_name = '{}_{}'.format(typ,jet)
+                  h1[pbin][flv][jet][typ] = h_dir.Get(h_name)
+                  h1[pbin][flv][jet][typ].SetName('{}_{}'.format(h_name,pbin))
+                  h1[pbin][flv][jet][typ].SetDirectory(0) # Important!
+                  if lumi_scale:
+                     h1[pbin][flv][jet][typ].Scale(ls)
+         tfile.Close()
+         
+      self.m_histos_dijet_proc = h1
+      self.m_dijets = jets
+         
+      h1_add_proc = {}
+      for f,flv in enumerate(flv_all):
+         h1_add_proc[flv] = {}
+         for jet in jets:
+            h1_add_proc[flv][jet] = {}
+            for typ in h_types:
+               h_name = typ+'_'+jet+'_'+flv
+               for b,pbin in enumerate(pbins):
+                  if b == 0:
+                     h1_add_proc[flv][jet][typ] = h1[pbin][flv][jet][typ].Clone(h_name)
+                  else:
+                     h1_add_proc[flv][jet][typ].Add(h1[pbin][flv][jet][typ])
+                  h1_add_proc[flv][jet][typ].SetDirectory(0) # Important!
+               
+               
+      self.m_histos_dijet = h1_add_proc
+      
+############################################################################      
 
 class BTagEfficiency:
    def __init__(self,process,btag_dir='btag_jets',nobtag_dir='all_jets',h1_pt='pt_jet',h2_pt_eta='pt_eta_jet',rebin=None):
@@ -199,98 +369,98 @@ class BTagEfficiency:
    def flavours(self):
       return self.m_flavours
 
-class Process:
-   def __init__(self,alias,directory):
-      self.m_alias = alias
-      self.m_dir = directory
-      xs = {}
-      if self.m_alias=='QCD_HT':
-         xs['100to200']   = 23700000.0
-         xs['200to300']   = 1547000.0
-         xs['300to500']   = 322600.0
-         xs['500to700']   = 29980.0
-         xs['700to1000']  = 6334.0
-         xs['1000to1500'] = 1088.0
-         xs['1500to2000'] = 99.11
-         xs['2000toInf']  = 20.23
-      if self.m_alias=='bEnriched':
-         xs['100to200']   = 23700000.0
-         xs['200to300']   = 1547000.0
-         xs['300to500']   = 322600.0
-         xs['500to700']   = 29980.0
-         xs['700to1000']  = 6334.0
-         xs['1000to1500'] = 1088.0
-         xs['1500to2000'] = 99.11
-         xs['2000toInf']  = 20.23
-      if self.m_alias=='BGenFilter':
-         xs['100to200']   = 23700000.0
-         xs['200to300']   = 1547000.0
-         xs['300to500']   = 322600.0
-         xs['500to700']   = 29980.0
-         xs['700to1000']  = 6334.0
-         xs['1000to1500'] = 1088.0
-         xs['1500to2000'] = 99.11
-         xs['2000toInf']  = 20.23
-      if self.m_alias=='QCD_Mu':
-         xs['50To80']    = 376600.0
-         xs['80To120']   = 88930.0
-         xs['120To170']  = 21230.0
-         xs['170To300']  = 7055.0
-         xs['300To470']  = 619.3
-         xs['470To600']  = 59.24
-         xs['600To800']  = 18.21
-         xs['800to1000'] = 3.275
-         xs['1000toInf'] = 1.078
-      self.m_xsections = xs
-      self.m_bins = xs.keys()
-      #root files
-      rf = {}
-      ngen = {}
-      lumi_scale = {}
-      if len(xs) > 0:
-         keyword = self.m_alias
-         if self.m_alias == 'QCD_Mu':
-            keyword = 'MuEnrichedPt5'
-         ld = glob.glob(directory+'/*'+keyword+'*.root')
-         for b in self.m_bins:
-            f = [s for s in ld if b in s]
-            if len(f) == 0:
-               print('No file for bin '+b+'. Skipping!')
-               continue
-            if len(f) > 1:
-               print('There is more than 1 file with bin '+b+'. The first one, '+f[0]+' will be considered')
-            rf[b] = f[0]
-            # get number of generator+pileup weighted generated events
-            hfile = TFile( f[0], 'OLD' )
-            h_wf = hfile.Get('workflow')
-            ngen[b] = h_wf.GetBinContent(3)
-            lumi_scale[b] = xs[b]/ngen[b]
-            hfile.Close()
-      self.m_rootfiles = rf
-      self.m_ngen = ngen
-      self.m_lumi_scale = lumi_scale
-      # scale to same lumi
-      
-      
-      
-   def alias(self):
-      return self.m_alias
-      
-   def crossSections(self):
-      return self.m_xsections
-      
-   def bins(self):
-      return self.m_bins
-      
-   def neventsGenerated(self):
-      return self.m_ngen
-         
-   def rootFiles(self):
-      return self.m_rootfiles
-      
-   def luminosityScale(self,lumi=1000.):
-      ls = {}
-      for b in self.m_bins:
-         ls[b] = self.m_lumi_scale[b]*lumi
-      return ls
-
+# class Process:
+#    def __init__(self,alias,directory):
+#       self.m_alias = alias
+#       self.m_dir = directory
+#       xs = {}
+#       if self.m_alias=='QCD_HT':
+#          xs['100to200']   = 23700000.0
+#          xs['200to300']   = 1547000.0
+#          xs['300to500']   = 322600.0
+#          xs['500to700']   = 29980.0
+#          xs['700to1000']  = 6334.0
+#          xs['1000to1500'] = 1088.0
+#          xs['1500to2000'] = 99.11
+#          xs['2000toInf']  = 20.23
+#       if self.m_alias=='bEnriched':
+#          xs['100to200']   = 1117000.0
+#          xs['200to300']   = 80430.0
+#          xs['300to500']   = 16620.0
+#          xs['500to700']   = 1487.0
+#          xs['700to1000']  = 296.5
+#          xs['1000to1500'] = 46.61
+#          xs['1500to2000'] = 3.72
+#          xs['2000toInf']  = 0.6462
+#       if self.m_alias=='BGenFilter':
+#          xs['100to200']   = 1282000.0
+#          xs['200to300']   = 111800.0
+#          xs['300to500']   = 28070.0
+#          xs['500to700']   = 3082.0
+#          xs['700to1000']  = 724.2
+#          xs['1000to1500'] = 138.2
+#          xs['1500to2000'] = 13.61
+#          xs['2000toInf']  = 2.909
+#       if self.m_alias=='QCD_Mu':
+#          xs['50To80']    = 376600.0
+#          xs['80To120']   = 88930.0
+#          xs['120To170']  = 21230.0
+#          xs['170To300']  = 7055.0
+#          xs['300To470']  = 619.3
+#          xs['470To600']  = 59.24
+#          xs['600To800']  = 18.21
+#          xs['800to1000'] = 3.275
+#          xs['1000toInf'] = 1.078
+#       self.m_xsections = xs
+#       self.m_bins = xs.keys()
+#       #root files
+#       rf = {}
+#       ngen = {}
+#       lumi_scale = {}
+#       if len(xs) > 0:
+#          keyword = self.m_alias
+#          if self.m_alias == 'QCD_Mu':
+#             keyword = 'MuEnrichedPt5'
+#          ld = glob.glob(directory+'/*'+keyword+'*.root')
+#          for b in self.m_bins:
+#             f = [s for s in ld if b in s]
+#             if len(f) == 0:
+#                print(alias+': No file for bin '+b+'. Skipping!')
+#                continue
+#             if len(f) > 1:
+#                print('There is more than 1 file with bin '+b+'. The first one, '+f[0]+' will be considered')
+#             rf[b] = f[0]
+#             # get number of generator+pileup weighted generated events
+#             hfile = TFile( f[0], 'OLD' )
+#             h_wf = hfile.Get('workflow')
+#             ngen[b] = h_wf.GetBinContent(3)
+#             lumi_scale[b] = xs[b]/ngen[b]
+#             hfile.Close()
+#       self.m_rootfiles = rf
+#       self.m_ngen = ngen
+#       self.m_lumi_scale = lumi_scale
+#       # scale to same lumi
+#       
+#       
+#       
+#    def alias(self):
+#       return self.m_alias
+#       
+#    def crossSections(self):
+#       return self.m_xsections
+#       
+#    def bins(self):
+#       return self.m_bins
+#       
+#    def neventsGenerated(self):
+#       return self.m_ngen
+#          
+#    def rootFiles(self):
+#       return self.m_rootfiles
+#       
+#    def luminosityScale(self,lumi=1000.):
+#       ls = {}
+#       for b in self.m_bins:
+#          ls[b] = self.m_lumi_scale[b]*lumi
+#       return ls
+# 
